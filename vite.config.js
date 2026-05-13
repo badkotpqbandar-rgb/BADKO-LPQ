a7152f87cc050d22189c3a07047f98cd94ff8a6e
badkotpqbandar-rgb <badkotpqbandar@gmail.com>
2026-05-13 22:11:14 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    // Memperbesar batas peringatan chunk menjadi 2MB (2000kb)
-    // Default bawaan Vite adalah 500kb
-    chunkSizeWarningLimit: 2000, 
+    // Menaikkan batas peringatan ukuran file menjadi 1600 kB (1.6 MB)
+    chunkSizeWarningLimit: 1600, 
+    
+    // (Opsional) Memisahkan library besar agar tidak menumpuk di 1 file
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('firebase')) return 'vendor-firebase';
+            if (id.includes('lucide')) return 'vendor-lucide';
+            return 'vendor'; // Library lainnya
+          }
+        }
+      }
+    }
   }
 })
