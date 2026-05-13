384aa4f805aed8f2213b8418f9b102815548bdea
badkotpqbandar-rgb <badkotpqbandar@gmail.com>
2026-05-13 21:28:43 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -74,12 +74,12 @@ const getEnv = (key, fallback) => {
 };
 
 const firebaseConfig = {
-  apiKey: getEnv("VITE_FIREBASE_API_KEY", "AIzaSyCHdwhrtWUgS46KL0IS1UB8cHGEEye-TCw"),
-  authDomain: getEnv("VITE_FIREBASE_AUTH_DOMAIN", "fasi-ix-82267.firebaseapp.com"),
-  projectId: getEnv("VITE_FIREBASE_PROJECT_ID", "fasi-ix-82267"),
-  storageBucket: getEnv("VITE_FIREBASE_STORAGE_BUCKET", "fasi-ix-82267.firebasestorage.app"),
-  messagingSenderId: getEnv("VITE_FIREBASE_MESSAGING_SENDER_ID", "1078063708798"),
-  appId: getEnv("VITE_FIREBASE_APP_ID", "1:1078063708798:web:7180f28c1224ea5965362f"),
+  apiKey: getEnv("VITE_FIREBASE_API_KEY", "AIzaSyDOxyFEDz8ri0pxM3vCzQGv2uRAMdUGRpg"),
+  authDomain: getEnv("VITE_FIREBASE_AUTH_DOMAIN", "fasi-2026-644ed.firebaseapp.com"),
+  projectId: getEnv("VITE_FIREBASE_PROJECT_ID", "fasi-2026-644ed"),
+  storageBucket: getEnv("VITE_FIREBASE_STORAGE_BUCKET", "fasi-2026-644ed.firebasestorage.app"),
+  messagingSenderId: getEnv("VITE_FIREBASE_MESSAGING_SENDER_ID", "142359313047"),
+  appId: getEnv("VITE_FIREBASE_APP_ID", "1:142359313047:web:68e4afb20f7109241587d2"),
 };
 
 const app = initializeApp(firebaseConfig);
